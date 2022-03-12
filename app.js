bef20d758bde5cc3de03a2bf88b8db9bdba1c172
duocnp <duocnguyen6799@gmail.com>
2022-03-12 17:30:08 +0700

    Remove express

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
 
-app.listen(3000, ()=>{
-    console.log('Server is listening port 3000')
-})
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
